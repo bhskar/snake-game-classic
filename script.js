5877fa9856480d613052a7aaa4316f44aad147a8
Bhaas-kar <110971795+bhskar@users.noreply.github.com>
2023-05-31 23:58:39 +0530

    added first function createCell()

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const gridSize = 20;
 const cellSize = 20;
 const initialSnakeLength = 4;
 const initialSpeed = 200;
+
+
+function createCell(x, y) {
+  const cell = document.createElement("div");
+  cell.style.width = cell.style.height = cellSize + "px";
+  cell.style.gridColumnStart = x + 1;
+  cell.style.gridRowStart = y + 1;
+  return cell;
+}
