cbd680c0aa60a4234982f422bece3f8e24d22dff
Bhaas-kar <110971795+bhskar@users.noreply.github.com>
2023-05-31 23:57:39 +0530

    starting with some variables to use

---
+++ script.js
@@ -0,0 +1,7 @@
+const board = document.getElementById("game-board");
+const scoreDisplay = document.getElementById("score");
+
+const gridSize = 20;
+const cellSize = 20;
+const initialSnakeLength = 4;
+const initialSpeed = 200;
